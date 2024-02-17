b581cf627a459cf865132ac983a0d11afd3d1ceb
samya-roy <samyaroy999@gmail.com>
2024-02-17 16:32:10 +0600

    scors showing redy

--- alphabet-clash.js
+++ alphabet-clash.js
@@ -2,12 +2,50 @@
 function continueGame() {
    const alpahabet = randomNumber();
    const getAlphabet = document.getElementById('rendom-alphabet');
-   getAlphabet.innerText = alpahabet;
-   setClassList(alpahabet.toLocaleLowerCase())
+   getAlphabet.innerText = alpahabet.toLowerCase();
+   setClassList(alpahabet.toLowerCase())
 }
 
 function play() {
     hideElement("home-screen");
     showElement("play-ground");
     continueGame();
+    const scoreSection = document.getElementById("score-section");
+    scoreSection.classList.add("hidden"); 
 }
+
+function hendelKeyUpEvent(event) {
+    const playerPressed= event.key;
+    const getAlphabet = document.getElementById('rendom-alphabet');
+    const randomAlphabet = getAlphabet.innerText.toLowerCase() ;
+    console.log(playerPressed, randomAlphabet);
+
+    if (playerPressed === randomAlphabet) {
+        console.log("you get a point");
+        const crentScore = document.getElementById("crentScore")
+        const crentScoreText = crentScore.innerText;
+        const crentScores = parseInt(crentScoreText);
+        let crent = crentScores + 1;        
+        crentScore.innerText = crent;
+
+        const tolalScorc = document.getElementById("tolal-scorc");
+        tolalScorc.innerText = crent;
+        removeBackground(randomAlphabet);
+        continueGame();
+    }
+    else{
+        console.log("you missed, loseed a life" );
+        const crentLife = document.getElementById("crentLife")
+        const crentLifeText = crentLife.innerText;
+        const crentLifes = parseInt(crentLifeText);
+        const crent = crentLifes - 1;  
+        crentLife.innerText = crent;
+        if (crent === 0) {
+            const playGround = document.getElementById("play-ground");
+            playGround.classList.add("hidden");
+            const scoreSection = document.getElementById("score-section");
+            scoreSection.classList.remove("hidden");           
+        }
+    }
+}
+document.addEventListener("keyup",hendelKeyUpEvent)
--- ulti.js
+++ ulti.js
@@ -2,14 +2,17 @@ function hideElement(element) {
    const  elementAdd =  document.getElementById(element)
    elementAdd.classList.add("hidden");
 }
+
 function showElement(element) {
    const   elementRemove =  document.getElementById(element)
    elementRemove.classList.remove("hidden");
 }
+
 function randomNumber() {
-    const alphabetsString = "A B C D E F G H I K L M N O P Q R S T V X Y Z" ;
-    const alphabetsAray = alphabetsString.split(' ');
-    const randomNum  = Math.round(Math.random() * 25 + 1);
+    const alphabetsString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" ;
+    const alphabetsAray = alphabetsString.split('');
+    const randomNum  = Math.round(Math.random() * 25 );
+    console.log(randomNum);
     const alpahabet = alphabetsAray[randomNum];
     return alpahabet;  
 }
@@ -17,6 +20,11 @@ function randomNumber() {
 function setClassList(element) {
    const  elementBg =  document.getElementById(element)
    elementBg.classList.add("bg-amber-700")
+}
+
+function removeBackground(element) {
+   const  elementBg =  document.getElementById(element)
+   elementBg.classList.remove("bg-amber-700")
 }
 
 
